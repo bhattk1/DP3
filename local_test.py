30b1ebc6e2ffa3427916a31af9ada623e1db246b
Shtayo <fx_liu@hotmail.com>
2023-02-22 15:27:02 -0500

    Added passing values from processes to main

--- local_test.py
+++ local_test.py
@@ -6,28 +6,52 @@ import time
 
 from multiprocessing import Process as proc
 
-def standardListInit(rolling,total):
-    standardList = ListTemp(0)
-    standardList.getRollingTemp(rolling,total)
-    print(standardList.rollinglist)
+from multiprocessing import Value as val
 
-def injuredListInit(rolling,total):
-    injuredList = ListTemp(1)
+standardList = ListTemp(0)
+injuredList = ListTemp(1)
+
+def standardListInit(rolling,total,rolling_avg):
+    standardList.getRollingTemp(rolling,total)
+    ##print(standardList.rollinglist)
+    ri = standardList.rollinglist
+
+    standard_avg = 0
+
+    for x in standardList.rollinglist:
+        standard_avg += x
+
+    print(standard_avg)
+    
+    rolling_avg.value = standard_avg/len(standardList.rollinglist)
+
+def injuredListInit(rolling,total,rolling_avg):
     injuredList.getRollingTemp(rolling,total)
     print(injuredList.rollinglist)
+    injured_avg = 0
+
+    for x in injuredList.rollinglist:
+        injured_avg += x
+
+    print(injured_avg)
+    
+    rolling_avg.value = injured_avg/len(injuredList.rollinglist)
 
 if __name__ == "__main__":
+
+    svalue = val('d',0.0)
+    ivalue = val('d',0.0)
 
     start = time.perf_counter()
     jobs = []
     process1 = proc(
             target=standardListInit,
-        args=(2,10)
+        args=(2,10,svalue)
     )
     jobs.append(process1)
     process2 = proc(
             target=injuredListInit,
-        args=(2,10)
+        args=(2,10,ivalue)
     )
     jobs.append(process2)
 
@@ -37,9 +61,14 @@ if __name__ == "__main__":
     for j in jobs:
         j.join()
 
-    for j in jobs:
-        j.terminate()
-
     stop = time.perf_counter()
 
     print(stop-start)
+
+    print(svalue.value)
+    print(ivalue.value)
+
+    if svalue.value < ivalue.value:
+        print("Servo activated")
+    else:
+        print("Go next")
